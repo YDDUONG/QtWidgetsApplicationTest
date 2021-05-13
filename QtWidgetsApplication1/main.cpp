2342514646aeac079b1d098bc49605e7bbbac8da
Admin <Admin@DESKTOP-DLEJCJJ>
2021-05-13 13:42:07 +0800

    first commit

--- QtWidgetsApplication1/main.cpp
+++ QtWidgetsApplication1/main.cpp
@@ -1,10 +1,12 @@
 #include "QtWidgetsApplication1.h"
 #include <QtWidgets/QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QtWidgetsApplication1 w;
+	std::cout << "hello" << std::endl;
     w.show();
     return a.exec();
 }
